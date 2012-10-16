732da050b6c49a2abb798dc406ed84df005a456a
Johann Philipp Strathausen (on eeepc) <strathausen@gmail.com>
2012-10-16 09:44:50 +0200

    new compile

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
 /*
 
-mumpitz - all the blog engine you will ever need
+Mumpitz - all the blog engine you will ever need
 
 @author Johann Philipp Strathausen <strathausen@gmail.com>
 */
-var Mumpitz, async, schnauzer, yamlmd, _;
+var Mumpitz, async, connect, defaultTo, es, fs, intercept, mu, path, router, schnauzer, yamlmd, _;
 
 require('coffee-script');
 
 _ = require('underscore');
 
+fs = require('fs');
+
+path = require('path');
+
 async = require('async');
+
+es = require('event-stream');
+
+connect = require('connect');
 
 yamlmd = require('yamlmd');
 
 schnauzer = require('schnauzer');
+
+defaultTo = require('./plugins/defaultTo');
+
+intercept = require('./plugins/intercept');
+
+router = require('./plugins/router');
 
 Mumpitz = (function() {
 
@@ -25,6 +39,7 @@ Mumpitz = (function() {
     if (!properties.dir) {
       throw new Error('please specify "dir" where your articles are');
     }
+    this.defaults = properties;
     this.blog = new (Blog = (function() {
 
       function Blog() {}
@@ -35,16 +50,48 @@ Mumpitz = (function() {
     _.defaults(this.blog, properties);
     _.defaults(this.blog, {
       documents: [],
-      template: __dirname + '/theme/article.mustache'
+      template: __dirname + '/example/theme/article.hbs',
+      app: connect()
     });
+    this.router = router(this.blog.app);
   }
 
   Mumpitz.prototype.go = function(cb) {
-    return async.series(plugins.map(function(p) {
-      return async.apply(p, blog);
-    }), cb);
+    var EXTREG,
+      _this = this;
+    EXTREG = /\.(md|markdown|yamlmd)$/;
+    return fs.readdir(this.blog.dir, function(err, docs) {
+      if (err) return cb(err);
+      return async.forEach(docs, (function(doc, cb) {
+        var readStream, x;
+        if (!EXTREG.test(doc)) return cb();
+        x = {};
+        readStream = fs.createReadStream(path.join(_this.blog.dir, doc));
+        return readStream.pipe(yamlmd.stream()).pipe(defaultTo(_this.blog)).pipe(defaultTo({
+          id: doc.replace(EXTREG, '')
+        })).pipe(intercept(function(item) {
+          return _this.blog.documents.push(item);
+        })).pipe(intercept(function(item) {
+          return x.url = item.id;
+        })).pipe(schnauzer.stream()).pipe(_this.router(x)).on('end', function() {
+          console.log('url:', x.url);
+          return cb();
+        });
+      }), cb);
+    });
   };
 
   return Mumpitz;
 
 })();
+
+if (!module.parent) {
+  mu = new Mumpitz({
+    dir: __dirname + '/example/articles',
+    layout: __dirname + '/example/theme/layout.hbs'
+  });
+  mu.go(function() {
+    console.error('done');
+    return mu.blog.app.listen(3000);
+  });
+}
